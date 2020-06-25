6b818704c2d9cbdb14dca03c0047e8b5b1b37736
maronara <67179199+maronara@users.noreply.github.com>
2020-06-24 21:13:12 -0700

    Add files via upload

---
+++ EncryptionDecryptionProgram.py
@@ -0,0 +1,29 @@
+alphabet = 'abcdefghijklmnopqrstuvwxyz'
+key = 3
+newMessage = ''
+
+message = input('Please enter a message: ')
+
+for character in message:
+    position = alphabet.find(character)
+    newPosition = (position + key) % 26
+    newCharacter = alphabet[newPosition]
+
+    newMessage += newCharacter
+
+print('Your new message is', newMessage)
+
+alphabet = 'abcdefghijklmnopqrstuvwxyz'
+key = -3
+newMessage = ''
+
+message = input('Please enter an encrypted message: ')
+
+for character in message:
+    position = alphabet.find(character)
+    newPosition = (position + key) % 26
+    newCharacter = alphabet[newPosition]
+
+    newMessage += newCharacter
+
+print('Your decrypted message is', newMessage)
